4b79b79b11a01e789bd4b6669949b76cfdf5214a
Saurabh Singh <singh9761080683saurabh@gmail.com>
2019-12-25 16:38:28 +0530

    Topic	: Naive String Search Algorithm #3 Issue	: https://github.com/saurabh8859/PythonBoilerPlate/issues/3 Status	: Done

--- pythonBoilerPlate.py
+++ pythonBoilerPlate.py
@@ -1,12 +1,55 @@
-#this is my python boiler plate
-def testingInput():
-    get_str = lambda: input()
-    get_int = lambda: int(input())
-    get_list_int = lambda: [*map(int,input().split())]
-    get_list_int_newLine = lambda n: [int(input()) for i in range(n)]
-    get_2D_list_int = lambda n,m: [[*map(int,input().split())] for i in range(n)]
-    get_list_char = lambda: input().split()
-    get_list_comma_seprated_int = lambda: [*map(int,input().split(","))]
-    convert_str_to_list = lambda str1: list(str1)
-    convert_str_to_int = lambda str1: int(str1)
-    convert_int_to_str = lambda int1: str(int1)
+# this is my python boiler plate
+
+
+class TestingInput:  # Class for input and conversion
+    def __init__(self):
+        pass
+
+    def get_str(self):  # For string Input
+        return input()
+
+    def get_int(self):  # For integer Input
+        return int(input())
+
+    def get_list_int(self):  # For Integer List Input Space seprated
+        return [*map(int, input().split())]
+
+    def get_list_int_newLine(self, n):  # For Integer List Input Line By Line
+        return [int(input()) for i in range(n)]
+
+    def get_2D_list_int(self, n, m):  # For 2d list Input
+        return [[*map(int, input().split())] for i in range(n)]
+
+    def get_list_char(self):  # For character list input
+        return input().split()
+
+    def get_list_comma_seprated_int(self):  # For comma seprated list input
+        return [*map(int, input().split(","))]
+
+    def convert_str_to_list(self, str1):  # For str to list
+        return list(str1)
+
+    def convert_str_to_int(self, str1):  # For string to integer
+        return int(str1)
+
+    def convert_int_to_str(self, int1):  # For integer to string
+        return str(int1)
+
+
+class StringSearch(TestingInput):  # string match or pattern match
+    def __init__(self):
+        TestingInput.__init__(self)
+
+    # Naive String Searching Algorithm
+    # str1 is string and ptr is to match in str1
+    def naiveStringSearching(self, str1, ptr):
+        N = len(str1)  # length of string
+        M = len(ptr)  # lenght of pattern
+        for i in range(N-M+1):
+            j = 0
+            while j < M and ptr[j] == str1[i+j]:
+                j += 1
+            if j == M:
+                return "Pattern Found At index " + self.convert_int_to_str(i)
+        else:
+            return "Pattern not found"
